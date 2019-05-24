fa42495b86d850d088c3a0d5e8e8da38e03d9fc9
NCJo <cnimkulr@gmail.com>
2019-05-23 21:58:04 -0600

    Initialize Repository

---
+++ src/components/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { startGame, cancelGame } from '../actions/settings';
+
+
+class App extends Component {
+    
+    render() { 
+        console.log('this', this);
+
+        return (
+            <div>
+                <h2>♠️ ♦️ Evens or Odds ♥️ ♣️</h2>
+                {
+                    this.props.gameStarted ? (
+                        <div>
+                            <h3>The game is on!</h3>
+                            <br />
+                            <button onClick={this.props.cancelGame}>I'm not ready yet!</button>
+                        </div>
+                    ) : (
+                        <div>
+                            <h3>A new game awaits!</h3>
+                            <br />
+                            <button onClick={this.props.startGame}>Start Game!</button>
+                        </div>
+                    )
+                }
+            </div>
+            
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    console.log('state', state);
+
+    return { gameStarted: state.gameStarted };
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        startGame: () => dispatch(startGame()),
+        cancelGame: () => dispatch(cancelGame())
+    };
+}
+
+const componentConnector = connect(mapStateToProps, mapDispatchToProps);
+
+export default componentConnector(App);
